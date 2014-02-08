983dbece6ce1a310f55461c1c0ab76da63899be5
Walter Lucetti <myzhar@robot-home.it>
2014-02-08 11:29:50 +0100

    Modify Exception Handling #5
    / Solution to issue #5

--- HLU/RoboController-PID_Regulator/main.cpp
+++ HLU/RoboController-PID_Regulator/main.cpp
@@ -2,6 +2,35 @@
 #include <QApplication>
 #include <QSettings>
 
+class MyApplication : public QApplication
+{
+public:
+    MyApplication(int& argc, char** argv) :
+        QApplication(argc, argv) {}
+
+    virtual ~MyApplication() { }
+
+    bool notify(QObject* receiver, QEvent* event)
+    {
+        bool done = true;
+        try
+        {
+            done = QApplication::notify(receiver, event);
+        }
+        catch(roboctrl::RcException &e)
+        {
+            qCritical() << QObject::tr("RoboController Exception: %1").arg(e.getExcMessage());
+            exit( EXIT_FAILURE );
+        }
+        catch(...)
+        {
+            qCritical() << tr("Unhandled exception!");
+            exit( EXIT_FAILURE );
+        }
+        return done;
+    }
+};
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
